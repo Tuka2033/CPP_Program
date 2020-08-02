fd50471cf5a9f9fba43b72161aaad13ef39a43e4
Tuka2033 <63064476+Tuka203>
2020-08-02 14:18:54 -0700

    Assignment

--- Aug_2/Aug_2-2/Source.cpp
+++ Aug_2/Aug_2-2/Source.cpp
@@ -37,13 +37,70 @@ void Reverse(char* s)
 	}
 	rev[j] = '\0';
 	cout << "\nReverse String is::"<<rev;
-	if (strcmp(s, rev))
+}
+void strConcat(char* str1, char* str2)
+{
+	while (*str1 != '\0')
 	{
-		cout << "\nGiven String is Palindrome....!";
+		str1++;
+	}
+
+	while (*str2 != '\0')
+	{
+		*str1 = *str2;
+		str1++;
+		str2++;
+	}
+}
+void chkPalindrome(char* str)
+{
+	char* START = str;
+	char* END = str;
+	while (*END != '\0')
+	{
+		END++;
+	}
+	END--;
+	while (START < END)
+	{
+		if (*START != *END)
+		{
+			break;
+		}
+
+		START++;
+		END--;
+	}
+
+	if (START < END)
+	{
+		cout << "\nSTRING IS NOT PALINDROME";
 	}
 	else
 	{
-		cout << "\nGiven String is not Palindrome....!";
+		cout << "\nSTRING IS PALINDROME";
+	}
+}
+
+void strCompare(char* src, char* dest)
+{
+	while ((*src != '\0') && (*dest != '\0'))
+	{
+		if (*src != *dest)
+		{
+			break;
+		}
+		src++;
+		dest++;
+	}
+
+	if ((*src == '\0') && (*dest == '\0'))
+	{
+		cout << "\nSTRING ARE SAME";
+	}
+	else
+	{
+		cout << "\nSTRING ARE NOT SAME";
 	}
 }
 int main()
@@ -54,13 +111,28 @@ int main()
 	char str3[10],str4[20];
 	cout << "\nEnter a string::";
 	cin >> str1;
+
+	//length of String
 	len = length(str1);
 	cout << "\nLength of String::" << len;
+
+
+	//copy of String
 	copy(str1, str2);
 	cout << "\nSecond String is::" << str2;
+
+	//compare
+	strCompare(str1, str2);
+	
+	//Reverse
 	Reverse(str2);
 	cout << "\nEnter 3rd String::";
 	cin >> str3;
+
+	//check Palindrome
+	chkPalindrome(str3);
+
+	//String Concantation 
 	strcpy(str4, str1);
 	strcat(str4, " ");
 	strcat(str4, str3);
